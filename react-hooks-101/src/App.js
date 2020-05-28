fbba60e7b2b143919f3660dedfe36c8a68843361
kou1127h <pg5027ace@gmail.com>
2020-05-28 21:34:57 +0900

    useState

--- react-hooks-101/src/App.js
+++ react-hooks-101/src/App.js
@@ -1,5 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const App = () => <div>ppp</div>;
+const App = () => {
+  const [count, setCount] = useState(0);
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+
+  const increment2 = () => setCount((previosuCount) => previosuCount + 1);
+  const decrement2 = () => setCount((previosuCount) => previosuCount - 1);
+
+  const reset = () => setCount(0);
+  const double = () => setCount((previosuCount) => previosuCount * 2);
+  const divide3 = () =>
+    setCount((previosuCount) =>
+      previosuCount % 3 === 0 ? previosuCount / 3 : previosuCount
+    );
+  return (
+    <>
+      <div>count: {count}</div>
+      <div>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+      </div>
+      <div>
+        <button onClick={increment2}>+1</button>
+        <button onClick={decrement2}>-1</button>
+      </div>
+      <div>
+        <button onClick={reset}>Reset</button>
+        <button onClick={double}>x2</button>
+        <button onClick={divide3}>/3</button>
+      </div>
+    </>
+  );
+};
 
 export default App;
